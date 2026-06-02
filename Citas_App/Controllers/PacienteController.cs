a0df86198a358214b49a3ed14dec79b7940fa5c2
Joako601 <joaquin.uriona@tecdesoftware.edu.mx>
2026-06-02 12:59:00 -0600

    Agregar archivos de proyecto.

---
+++ Citas_App/Controllers/PacienteController.cs
@@ -0,0 +1,23 @@
+﻿using Citas_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Citas_App.Controllers
+{
+	public class PacienteController : Controller
+	{
+		private static List<Paciente> _pacientes = new()
+		{
+			new Paciente { Id = 1, Nombre = "Ana",   Apellido = "García",   Email = "ana@mail.com",   Telefono = "555-0001" },
+			new Paciente { Id = 2, Nombre = "Luis",  Apellido = "Martínez", Email = "luis@mail.com",  Telefono = "555-0002" },
+			new Paciente { Id = 3, Nombre = "María", Apellido = "López",    Email = "maria@mail.com", Telefono = "555-0003" },
+		};
+
+		public IActionResult Index() => View(_pacientes);
+
+		public IActionResult Detalle(int id)
+		{
+			var paciente = _pacientes.FirstOrDefault(p => p.Id == id);
+			return paciente == null ? NotFound() : View(paciente);
+		}
+	}
+}
---
+++ Citas_App/Views/Cita/PorPaciente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Citas_App.Views.Cita
+{
+    public class PorPacienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
